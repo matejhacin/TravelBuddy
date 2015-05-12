e3de244920fc6b870b3a7e60905bc9b3776e9c31
Matej <matej.hacin@gmail.com>
2015-05-12 12:17:57 +0200

    Created activities

---
+++ app/src/main/java/com/matejhacin/travelbuddy/MapViewActivity.java
@@ -0,0 +1,28 @@
+package com.matejhacin.travelbuddy;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MapViewActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
