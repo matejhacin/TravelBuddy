16dd9139bf75908544d9872e089315e4578d4b22
Matej <matej.hacin@gmail.com>
2015-05-27 14:42:21 +0200

    User can now see marker info and mark it done

--- app/src/main/java/com/matejhacin/travelbuddy/activities/DestinationInfoActivity.java
+++ app/src/main/java/com/matejhacin/travelbuddy/activities/DestinationInfoActivity.java
@@ -4,27 +4,61 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.matejhacin.travelbuddy.R;
+import com.matejhacin.travelbuddy.classes.DestinationMarker;
+import com.matejhacin.travelbuddy.database.DatabaseHandler;
 
 
-public class DestinationInfoActivity extends ActionBarActivity {
+public class DestinationInfoActivity extends ActionBarActivity implements View.OnClickListener {
+
+    /*
+    Variables
+     */
+
+    private TextView addressTextView;
+    private TextView infoTextView;
+
+    private DestinationMarker destinationMarker;
+
+    /*
+    Lifecycle
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination_info);
+
+        // Get extras
+        destinationMarker = (DestinationMarker) getIntent().getExtras().get("marker");
+
+        // Initialize variables
+        addressTextView = (TextView) findViewById(R.id.addressTextView);
+        infoTextView = (TextView) findViewById(R.id.infoTextView);
+
+        // Listener
+        findViewById(R.id.markDoneButton).setOnClickListener(this);
+
+        // Set text
+        getSupportActionBar().setTitle(destinationMarker.getTitle());
+        addressTextView.setText(destinationMarker.getAddress());
+        infoTextView.setText(destinationMarker.getInfo());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_destination_info, menu);
-        return true;
-    }
+    /*
+    Callbacks
+     */
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+    public void onClick(View v) {
+        if (v.getId() == R.id.markDoneButton) {
+            // Mark marker as done in DB
+            new DatabaseHandler(getApplicationContext()).setMarkerDone(destinationMarker);
+            finish();
+        }
     }
 }
